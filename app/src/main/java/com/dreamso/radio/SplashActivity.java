b0ba71de3c72a9528fce67c5a925e408397f1749
chashikajw <chashikajw007@gmail.com>
2019-05-24 02:38:13 +0530

    add splash screen

---
+++ app/src/main/java/com/dreamso/radio/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.dreamso.radio;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashActivity extends AppCompatActivity {
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); // hide the title bar
+        setContentView(R.layout.activity_splash);
+
+
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
